941cf7dfa7d05167b81c94703caa6db9e0995c13
mohammedsbd <mohammed1112ok@gmail.com>
2026-02-28 10:52:17 -0800

    add model resource and controller

--- react-crud/resources/js/components/app-sidebar.tsx
+++ react-crud/resources/js/components/app-sidebar.tsx
@@ -1,5 +1,6 @@
 import { Link } from '@inertiajs/react';
-import { BookOpen, FolderGit2, LayoutGrid } from 'lucide-react';
+import { BookOpen, FolderGit2, LayoutGrid, } from 'lucide-react';
+import { ShoppingCart } from 'lucide-react';
 import AppLogo from '@/components/app-logo';
 import { NavFooter } from '@/components/nav-footer';
 import { NavMain } from '@/components/nav-main';
@@ -25,7 +26,7 @@ const mainNavItems: NavItem[] = [
     {
         title: 'Manage Products',
         href: dashboard(),
-        icon: LayoutGrid,
+        icon: ShoppingCart,
     },
 ];
 
