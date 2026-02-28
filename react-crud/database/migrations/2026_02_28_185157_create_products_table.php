b6fae64059a7c2559bcce84d724f0598f8545994
mohammedsbd <mohammed1112ok@gmail.com>
2026-02-28 11:01:08 -0800

    migration done

--- react-crud/database/migrations/2026_02_28_185157_create_products_table.php
+++ react-crud/database/migrations/2026_02_28_185157_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable();
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2)->nullable();
+            $table->string('featured-image')->nullable();
+            $table->string('featured-image-original-name')->nullable();
             $table->timestamps();
         });
     }
